a78824a7a8e93c78c0aee500c9c15745a5786903
martinrrosero70-web <martinr.rosero70@gmail.com>
2026-01-16 19:12:24 -0500

    descripcion de que hace cada cosa
    pilas

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-from difflib import restore
-from nicegui import ui
+from difflib import restore# Esta importación parece innecesaria y podría ser un error; difflib se usa para comparar secuencias, pero no se utiliza en el código.
+from nicegui import ui  # Importa la biblioteca NiceGUI para crear interfaces de usuario web con Python.
 
 # Funciones
-def sumar():
+def sumar(): #realiza la suma de dos numeros 
     n1 =float(num1.value)
     n2 =float(num2.value)
     res = n1 + n2  
     resultado.set_text(f"resultado: {res}")
 
-def restar():
+def restar(): #realiza la resta de 2 numeros
     n1 =float(num1.value)
     n2 =float(num2.value)
     res = n1 - n2  
     resultado.set_text(f"resultado: {res}")  
 
 # Estructuras de la interfaz de usuario
-with ui.card().classes("w-60 m-auto p-4"):
+with ui.card().classes("w-60 m-auto p-4"): #crea una tarjeta centrada con un ancho de 60 unidades, marguen automatico. 
     ui.label("cALCULADORA").classes("font-serif w-full text-center")
     num1 = ui.input("numero 1").classes("w-full")
     num2 = ui.input("numero 2").classes("w-full")
-    bt_sumar = ui.button("SUMAR").classes("w-full bg-bleu-500 text-white").on ("click", sumar)
-    bt_restar = ui.button("restar").classes("w-full bg-bleu-500 text-white").on ("click", restar)
+    bt_sumar = ui.button("SUMAR").classes("w-full bg-blue-500 text-white").on ("click", sumar)
+    bt_restar = ui.button("restar").classes("w-full bg-blue-500 text-white").on ("click", restar)
     resultado = ui.label("resultado: ").classes("w-full text-center mt-4 ")
 
     ui.run()
